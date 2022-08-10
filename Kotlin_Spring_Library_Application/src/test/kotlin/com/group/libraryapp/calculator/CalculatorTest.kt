17f58f38d6e8dba5d899cf4f1f9452f36120b6f3
0n1dev <all_mind@naver.com>
2022-08-10 21:33:00 +0900

    feat: 순수 코틀린 코드 테스트

--- Kotlin_Spring_Library_Application/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ Kotlin_Spring_Library_Application/src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -2,7 +2,11 @@ package com.group.libraryapp.calculator
 
 fun main() {
     val calculatorTest = CalculatorTest()
-    calculatorTest.addTest();
+    calculatorTest.addTest()
+    calculatorTest.minusTest()
+    calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 class CalculatorTest {
@@ -19,5 +23,64 @@ class CalculatorTest {
         if (calculator.number != 8) {
             throw IllegalArgumentException()
         }
+    }
+
+    fun minusTest() {
+
+        //given
+        val calculator = Calculator(4)
+
+        // when
+        calculator.minus(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun multiplyTest() {
+
+        //given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.multiply(3)
+
+        // then
+        if (calculator.number != 15) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideTest() {
+
+        //given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(3)
+
+        // then
+        if (calculator.number != 1) {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun divideExceptionTest() {
+
+        //given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException("테스트 실패")
+        }
+
+        throw IllegalStateException("테스트 실패")
     }
 }
