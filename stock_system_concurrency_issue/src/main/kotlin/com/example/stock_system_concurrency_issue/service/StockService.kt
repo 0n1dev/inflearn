2f4b42b64ee997f93ce90243d0410e30a2e65c5a
0n1dev <all_mind@naver.com>
2022-08-28 23:13:41 +0900

    feat: Synchronized 이용해보기

--- stock_system_concurrency_issue/src/main/kotlin/com/example/stock_system_concurrency_issue/service/StockService.kt
+++ stock_system_concurrency_issue/src/main/kotlin/com/example/stock_system_concurrency_issue/service/StockService.kt
@@ -1,16 +1,17 @@
 package com.example.stock_system_concurrency_issue.service
 
-import com.example.stock_system_concurrency_issue.domain.Stock
 import com.example.stock_system_concurrency_issue.repository.StockRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
 class StockService(
     private val stockRepository: StockRepository
 ) {
 
-    fun decrease(id: Long, quantity: Long) {
+//    @Transactional
+    @Synchronized fun decrease(id: Long, quantity: Long) {
         val stock = stockRepository.findByIdOrNull(id) ?: throw RuntimeException()
 
         stock.decrease(quantity)
